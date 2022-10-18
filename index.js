8c7f25c4139e0f3ee4f31891dd2d0cf30157facd
stevehan12138 <stevehan12138@gmail.com>
2022-10-17 21:46:27 -0400

    Change port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const express = require('express')
 const { exec } = require("child_process");
 const fs = require('fs')
 const cors = require('cors')
 const fileUpload = require('express-fileupload');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
